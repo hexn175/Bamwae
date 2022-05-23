a9b01d2331e990974503a2f8dce19bea4a0dbeeb
hexn175 <hexn175@nenu.edu.cn>
2022-05-23 23:25:31 +0800

    getOPENID,getSessionKey

--- src/main/java/com/bluemsun/controller/UserController.java
+++ src/main/java/com/bluemsun/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.bluemsun.controller;
 
+import com.bluemsun.entity.WxUserInfo;
+import com.bluemsun.service.IWxService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    @Autowired
+    private IWxService wxService;
+    /***
+     * 获取到微信用户的 OPENID
+     */
     @RequestMapping("/login")
-    public String login() {
-        return "hello user";
-
+    public String login(@RequestParam("code") String code) throws Exception {
+        WxUserInfo wxInfo = this.wxService.getLoginCertificate(code);
+        //用户不存在，或者用户的信息不全
+        return wxInfo==null || wxInfo.getNickName()==null?"0":"1";
     }
 }
