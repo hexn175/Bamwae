c4c377cbb4c661c03d60594c44e920f0cb904b08
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-05-24 16:36:27 +0800

    @Controller

--- src/main/java/com/bluemsun/controller/UserController.java
+++ src/main/java/com/bluemsun/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.bluemsun.controller;
 
-
 import com.bluemsun.entity.User;
 import com.bluemsun.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
