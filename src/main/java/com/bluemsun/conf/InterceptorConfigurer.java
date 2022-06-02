d39e94613acdba3c0a8f5345a80b0237324480ed
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-06-02 13:46:27 +0800

    关闭登录检查

--- src/main/java/com/bluemsun/conf/InterceptorConfigurer.java
+++ src/main/java/com/bluemsun/conf/InterceptorConfigurer.java
@@ -1,5 +1,6 @@
 //package com.bluemsun.conf;
 //
+
 //import com.bluemsun.interceptor.LoginInterceptor;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.HandlerInterceptor;
